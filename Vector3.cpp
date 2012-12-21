aba00752ff866e80353827d72c86e1d45ab3fdaf
Yannick Lange <yannick_lange_94@hotmail.com>
2012-12-21 20:03:53 +0100

    Vector 3 class
    Vector3 class still some work to do:
    - printing of the vector3
    - Calculating distance between 2 vector3

---
+++ Vector3.cpp
@@ -0,0 +1,175 @@
+#include "Vector3.h"
+
+using namespace std;
+
+
+
+//*********************************************************
+//Constructors
+//*********************************************************
+//Vector3::Vector3(Vector2, float z);
+
+Vector3::Vector3(){
+	X = 0;
+	Y = 0;
+	Z = 0;
+}
+
+Vector3::Vector3(float value){
+	X = value;
+	Y = value;
+	Z = value;
+}
+
+
+Vector3::Vector3(float x, float y, float z){
+	X = x;
+	Y = y;
+	Z = z;
+}
+
+
+//*********************************************************
+//Operator Calculates
+//*********************************************************
+// operator *
+Vector3 Vector3::operator*(float scale){
+	return Vector3(X * scale, Y * scale, Z * scale);
+}
+
+// operator /
+Vector3 Vector3::operator/(float scale){
+	//We don't want to devide by 0. If it does, return an empty vector object
+	if(scale!=0)
+		return Vector3(X / scale, Y / scale, Z / scale);
+	else
+		return Vector3();
+}
+
+// operator +
+Vector3 Vector3::operator+(const Vector3& vec){
+	return Vector3(X + vec.X, Y + vec.Y, Z + vec.X);
+}
+
+// operator -
+Vector3 Vector3::operator-(const Vector3& vec){
+	return Vector3(X - vec.X, Y - vec.Y, Z - vec.X);
+}
+
+
+//Calcate references
+Vector3& Vector3::operator*=(float scale){
+	X *=scale;
+	Y *=scale;
+	Z *=scale;
+	return *this;	//Return reference of this pointer
+}
+
+Vector3& Vector3::operator/=(float scale){
+	if(scale!=0){
+		X /=scale;
+		Y /=scale;
+		Z /=scale;
+	}
+	return *this;	//Return reference of this pointer
+}
+
+Vector3& Vector3::operator+=(const Vector3& vec){
+	X+=vec.X;
+	Y+=vec.Y;
+	Z+=vec.Z;
+	return *this;	//Return reference of this pointer
+}
+Vector3& Vector3::operator-=(const Vector3& vec){
+	X-=vec.X;
+	Y-=vec.Y;
+	Z-=vec.Z;
+	return *this;	//Return reference of this pointer
+}
+
+//for comparing vectors
+bool Vector3::operator==(const Vector3 vec){
+	return (X==vec.X && Y==vec.Y && Z==vec.Z);
+}
+
+bool Vector3::operator!=(const Vector3 vec){
+	//this is a reference to the Vector3::operator==(const Vector3 vec).
+	return !(*this==vec);
+}
+
+
+//*********************************************************
+//Function Calculates
+//*********************************************************
+
+//Length
+float Vector3::Length(){
+	return sqrt(X*X + Y*Y + Z*Z);
+}
+
+//Dot
+float Vector3::DotProduct(const Vector3& vec){
+	return (X * vec.X + Y * vec.Y + Z * vec.Z);
+}
+
+	
+//Normalize
+void Vector3::Normalize(){
+	float len = Length();
+	if(len!=0){
+		X/=len;
+		Y/=len;
+		Z/=len;
+
+	}
+}
+		
+//change X, Y, Z in one function
+void Vector3::SetAll(float newX, float newY, float newZ){
+	X = newX;
+	Y = newY;
+	Z = newZ;
+}
+void Vector3::SetAll(Vector3& vec){
+	X = vec.X;
+	Y = vec.Y;
+	Z = vec.Z;
+}
+void Vector3::SetAll(Vector3 vec){
+	X = vec.X;
+	Y = vec.Y;
+	Z = vec.Z;
+}
+
+void Vector3::SetX(float value){
+	X = value;
+}
+void Vector3::SetY(float value){
+	Y = value;
+}
+void Vector3::SetZ(float value){
+	Z = value;
+}
+
+//Clamp
+
+//Cross ( not finished yet )
+//Vector3 Vector3::CrossProduct(const Vector3& vec){
+//	return ...
+//}
+
+//Distance between 2 vectors
+		
+
+//*********************************************************
+//Print the vector (does not work yet)
+//*********************************************************
+//friend ostream& Vector3::operator<<(ostream& output, cost Vector3& vec){
+//	output << vec.X << " " << vec.Y << " " << vec.Z <<endl;
+//}
+
+
+
+
+
+
---
+++ Vector3.h
@@ -0,0 +1,84 @@
+#ifndef VECTOR3_H_INCLUDED
+#define VECTOR3_H_INCLUDED
+
+#include <iostream>
+#include <cmath>
+
+class Vector3{
+	public:
+		//*********************************************************
+		//Fields
+		//*********************************************************
+		float X,Y,Z;
+
+
+		//*********************************************************
+		//Constructors
+		//*********************************************************
+		Vector3();
+		Vector3(float value);
+		//Vector3(Vector2, float z);
+		Vector3(float x, float y, float z);
+
+
+		//*********************************************************
+		//Operator Calculates
+		//*********************************************************
+		Vector3 operator*(float scale);				
+		Vector3 operator/(float scale);			
+		Vector3 operator+(const Vector3& vec);		
+		Vector3 operator-(const Vector3& vec);		
+
+		//Calcate references
+		Vector3& operator*=(float scale);		
+		Vector3& operator/=(float scale);		
+		Vector3& operator+=(const Vector3& vec);	
+		Vector3& operator-=(const Vector3& vec);	
+
+		//for comparing vectors
+		bool operator==(const Vector3 vec);
+		bool operator!=(const Vector3 vec);
+
+
+		//*********************************************************
+		//Function Calculates
+		//*********************************************************
+		//Length
+		float Length();
+		
+		//Clamp
+		
+		//Cross
+		Vector3 CrossProduct(const Vector3& vec);
+
+		//Distance between 2 vectors
+		
+		//Dot
+		float DotProduct(const Vector3& vec);
+		
+		//Normalize
+		void Normalize();
+		
+		//change X, Y, Z in one function
+		void SetAll(float newX, float newY, float newZ);
+		void SetAll(Vector3& vec);		//Get Vector3 reference 
+		void SetAll(Vector3 vec);		//Get Vector3 object
+		void SetX(float value);
+		void SetY(float value);
+		void SetZ(float value);
+
+		//*********************************************************
+		//Directions
+		//Examples: Up, down, left etc...
+		//*********************************************************
+
+
+		//*********************************************************
+		//Print the vector (does not work yet)
+		//*********************************************************
+		//friend std::ostream& operator<<(std::ostream& output, cost Vector3& vec);
+
+};
+
+
+#endif
